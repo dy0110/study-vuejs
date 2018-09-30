296b8064187cf67e8e2e3c2799308b0c228dd0fe
Daichi Yoshida <d.y.0110.barcelona@gmail.com>
2018-09-30 23:41:19 +0900

    データバインド

--- main.js
+++ main.js
@@ -7,29 +7,52 @@ var app = new Vue( {
     //アプリケーションで使用するデータ
     data:{
         message: 'Hello Vue.js!',
+        
         list:[ 'りんご', 'バナナ', 'いちご' ],
+        
         text: {
             text: "初期メッセージ",
             value: 'Hello Vue.js!'
+        
         },
+        
         number: number, //numberがリアクティブデータになる
+        
         show: true,
+        
         num: 1,
+        
         count: 0,
+        
         isChild: true,
+        
         isActive: true,
+        
         textColor: 'red',
+        
         bgColor: 'lightgray',
+        
         classObject: {
             child: true,
             'is-ctive': false,
         },
+        
         styleObject: {
             color: 'blue',
             backgroundColor: 'wheat'
         },
+        
         ok: true,
-        type: "A"
+        
+        type: "A",
+        
+        name: "キマイラ",
+
+        monster: [
+            { id: 1, name: "スライム", hp: 100 },
+            { id: 2, name: "ゴブリン", hp: 200 },
+            { id: 3, name: "ドラゴン", hp: 500 },
+        ]
     },
     //算出プロパティ
     computed:{
@@ -44,13 +67,35 @@ var app = new Vue( {
         click: function( event ){
             alert( "Click Button!" )
         },
+
         increment: function( ){
             this.count += 1;
+        },
+        //モンスター追加する関数
+        doAdd: function( ){
+            //list内で最大のIDを取得
+            var max = this.monster.reduce( 
+                function( a, b ){
+                    return a.id > b.id ? a.id : b.id
+                }, 0 
+            );
+            //モンスターリストに追加
+            this.monster.push( 
+                {
+                    id: max + 1,
+                    name: this.name,
+                    hp: 500
+                }
+             );
+        },
+
+        doRemove: function( index ){
+            this.monster.splice( index, 1 )
         }
     }
 } );
 //Vueオブジェクトへアクセス(app.data.messageではない)
-console.log( app.message )
+console.log( app.message );
 //リストに項目追加
 app.list.push( 'オレンジ' );
 //表示を隠す
